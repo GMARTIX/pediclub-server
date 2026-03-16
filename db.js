458ee0c7d2fc9226d99e2f929a5470042fc3e742
aguilatack <martin718g3@gmail.com>
2026-03-15 21:11:59 -0300

    Solución KeepAlive para la base de datos

--- db.js
+++ db.js
@@ -7,8 +7,10 @@ const pool = mysql.createPool({
   password: process.env.DB_PASSWORD || '',
   database: process.env.DB_NAME || 'turnos_db',
   waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0
+  connectionLimit: 5,
+  queueLimit: 0,
+  enableKeepAlive: true,
+  keepAliveInitialDelay: 10000
 });
 
 module.exports = pool;
